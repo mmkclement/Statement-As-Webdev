8c9fa71a0f0da52e7f6188329b799040f4efdbc6
mmkclement <maungmaungkha18290@gmail.com>
2023-04-21 12:38:19 +0200

    Nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-
-
+import NavBar from './components/NavBar';
 function App() {
   return (
-    <h1>Portfolio Project..</h1>
+       <NavBar />
+
   );
 }
 
