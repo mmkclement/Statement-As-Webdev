abcd206690888f49daa391f788d34d3921fcb209
mmkclement <maungmaungkha18290@gmail.com>
2023-04-23 19:34:16 +0200

    nav bar responsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,8 +5,10 @@ const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
       <div className="container">
+        <div className='logo-nav'>
 
         <a className="navbar-brand" href="#"><img className="logo" src={logo} alt="logo" /></a>
+        </div>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
