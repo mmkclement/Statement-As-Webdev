5ce93b884d71a480e0d693c6e7d3cd83e689a43c
mmkclement <maungmaungkha18290@gmail.com>
2023-04-24 14:59:06 +0200

    emmet react

---
+++ src/components/AboutMe.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutMe = () => {
+  return (
+    <div className='container py-5'>
+      <div className='row'>
+       <div className="row"></div>
+      </div>
+    </div>
+  )
+}
+
+export default AboutMe
