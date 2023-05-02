56e119ddb984633310c2dc660ee5a1d2753f634f
mmkclement <maungmaungkha18290@gmail.com>
2023-05-02 23:54:17 +0200

    React share icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,45 +1,57 @@
-import React from 'react'
-
+import React from "react";
+import {  FacebookIcon, FacebookShareButton, TwitterIcon, TwitterShareButton,LinkedinIcon,LinkedinShareButton} from "react-share";
 const Footer = () => {
   return (
-<div className="footer">
-<div className="container">
-  <div className="row">
-    <div className="col-lg-4 col-md-6 col-sm-6">
-      <div className="d-flex">
-        <p>city Paris Rue Carducci 75019</p>
-      </div>
-      <div className="d-flex">
-        <a href="tel: 0695505394"> +33 6 95 50 53 94</a>
-      </div>
-      <div className="d-flex">
-        <p>maungmaungkha18290@gmail.com</p>
+    <div className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4 col-md-6 col-sm-6">
+            <div className="d-flex">
+              <p>city Paris Rue Carducci 75019</p>
+            </div>
+            <div className="d-flex">
+              <a href="tel: 0695505394"> +33 6 95 50 53 94</a>
+            </div>
+            <div className="d-flex">
+              <p>maungmaungkha18290@gmail.com</p>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-2 col-sm-6">
+            <div className="row">
+              <div className="col">
+                <a href="" className="footer-nav">
+                  Home
+                </a>
+                <br />
+                <a href="" className="footer-nav">
+                  Aboutme
+                </a>
+                <br />
+                <a href="" className="footer-nav">
+                  Service
+                </a>
+                <br />
+              </div>
+              <div className="col">
+                <a href="" className="footer-nav">
+                  Experiences
+                </a>
+                <br />
+                <a href="" className="footer-nav">
+                  TextFromLove{" "}
+                </a>
+                <br />
+                <a href="" className="footer-nav">
+                  contact
+                </a>
+                <br />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    <div className="col-lg-3 col-md-2 col-sm-6">
-      <div className="row">
-        <div className="col">
-          <a href="" className="footer-nav">Home</a>
-          <br />
-          <a href="" className="footer-nav">Aboutme</a>
-          <br />
-          <a href="" className="footer-nav">Service</a>
-          <br />
-        </div>
-        <div className="col">
-          <a href="" className="footer-nav">Experiences</a>
-          <br />
-          <a href="" className="footer-nav">TextFromLove </a>
-          <br />
-          <a href="" className="footer-nav">contact</a>
-          <br />
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+  );
+};
 
-</div>  )
-}
-
-export default Footer
+export default Footer;
