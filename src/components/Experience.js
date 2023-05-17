fae78cc31fca99dbe222b9e5180fe5a081891e79
mmkclement <maungmaungkha18290@gmail.com>
2023-05-17 11:32:07 +0200

    contact form work well

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -14,10 +14,11 @@ const Experience = () => {
           <div className="timeline-content">
             <h3>2009-2016</h3>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
-              culpa ad dolor deleniti nobis totam maiores, accusamus soluta
-              ratione facere quos, tempora autem exercitationem repudiandae et?
-              Amet dolor ea fugiat?
+              During my impressive seven-year tenure at a Shwe FM broadcast
+              radio station, I played a vital role in ensuring a seamless
+              workflow in both the broadcast and transmission areas. My
+              responsibilities included handling satellite uplink and downlink
+              operations, as well as managing the network and broadcast systems.
             </p>
           </div>
         </div>
@@ -27,10 +28,11 @@ const Experience = () => {
           <div className="timeline-content">
             <h3>2016-2017</h3>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
-              culpa ad dolor deleniti nobis totam maiores, accusamus soluta
-              ratione facere quos, tempora autem exercitationem repudiandae et?
-              Amet dolor ea fugiat?
+              During my role as a technical consultant at the Institut Français,
+              I had the privilege of being involved in the prestigious Diplomacy
+              Festival organized by the French Ministry of Foreign Affairs and
+              the Myanmar Ministry of Foreign Affairs, known as the Mingalabar
+              Festival.
             </p>
           </div>
         </div>
